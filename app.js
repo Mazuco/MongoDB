560092d1a81cc5b59518b4718a7f116e13826ca6
Vitor Mazuco <vitor.mazuco@gmail.com>
2019-10-31 15:14:47 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-// teste.js
+// app.js
 // arquivo para teste de conexão ao MongoDB
 
 //requer o módulo "mongoClient" 
 var MongoClient = require('mongodb').MongoClient;
 
 //configurações do nosso mongodb
-var connectionUrl = 'mongodb://localhost:27017/meuprojeto',
-    sampleCollection = 'capitulos';
+var connectionUrl = 'mongodb://localhost:27017/meuprojeto', sampleCollection = 'capitulos';
 
 //Precisamos inserir esses conteúdos em nosso mongoDB
 var chapters = [{
-    'Titulo': 'Teste de Node',
+	'Titulo': 'Teste de Node',
     'Autor': 'Vitor Mazuco'
 },{
-    'Titulo': 'Teste de Mongo',
+	'Titulo': 'Teste de Mongo',
     'Autor': 'Vitor Mazuco'
 }];
 
-MongoClient.connect(connectionUrl, function(err, db) {
+MongoClient.connect(connectionUrl,{ useUnifiedTopology:true }, function(err, client) {
 
-  console.log("Conectado corretamente ao servidor");
+console.log("Conectado corretamente ao servidor");
+var db = client.db('meuprojeto');
 
-  // Obter alguma coleção
-  var collection = db.collection(sampleCollection);
+// Obter alguma coleção
+var collection = db.collection(sampleCollection);
 
-  collection.insert(chapters,function(error,result){
-    //aqui o resultado conterá uma matriz de registros inseridos
-    if(!error) {
-      console.log("Sucesso :"+result.ops.length+" capitulos inseridos!");
-    } else {
-      console.log("Algum erro foi encontrado!");
-    }
-    db.close();
+collection.insertMany(chapters,function(error,result){
+//aqui o resultado conterá uma matriz de registros inseridos
+if(!error) {
+	console.log("Sucesso :"+result.ops.length+" capitulos inseridos!");
+	} else {
+		console.log("Algum erro foi encontrado!");
+	}
+	client.close();
   });
 });
